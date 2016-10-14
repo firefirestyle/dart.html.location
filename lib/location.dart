b68e9a8ea161c62c4ef5c761c8537eba94ffcc6e
kyorohiro <kyorohiro@gmail.com>
2016-10-15 03:43:58 +0900

    ..

--- lib/location.dart
+++ lib/location.dart
@@ -1,44 +1,5 @@
+library location;
 import 'dart:html' as html;
 
-class Location {
-  String _hash = "";
-  String _baseAddress = "";
-  Map<String, String> _values = {};
-  Map<String, String> _urlValues = {};
-  Map<String, String> _hashValues = {};
-
-  String get hash => _hash;
-  String get baseAddress => _baseAddress;
-  Map<String, String> get values => _values;
-  Map<String, String> get urlValues => _urlValues;
-  Map<String, String> get hashValues => _hashValues;
-
-  Location() {
-    this._hash = html.window.location.hash;
-    {
-      var addr = html.window.location.href.replaceFirst(new RegExp("[#\?].*"), "");
-      this._baseAddress = addr.replaceFirst(new RegExp(r"/[^/]*$"), "/");
-    }
-    {
-      var prop1 = _hashValues = _prop(html.window.location.hash);
-      var prop2 = _urlValues = _prop(html.window.location.href.split("#")[0]);
-      for (String key in prop1.keys) {
-        _values[key] = prop1[key];
-      }
-      for (String key in prop2.keys) {
-        _values[key] = prop2[key];
-      }
-    }
-  }
-
-  Map<String, String> _prop(String hash) {
-    if (hash == null) {
-      return {};
-    }
-    Map<String, String> prop = {};
-    if (hash.indexOf("?") > 0) {
-      prop = Uri.splitQueryString(hash.substring(hash.indexOf("?") + 1));
-    }
-    return prop;
-  }
-}
+part 'src/location.dart';
+part 'src/page.dart';
---
+++ lib/src/page.dart
@@ -0,0 +1,62 @@
+part of location;
+
+enum PageManagerEvent { startLoading, stopLoading, }
+
+class PageManager {
+  static const int startLoading = 1;
+  static const int stopLoading = 2;
+
+  bool _isLoading = false;
+
+  List<Page> pages = [];
+  bool get isLoading => _isLoading;
+
+  PageManager() {
+    html.window.onHashChange.listen((_) {
+      var locationInfo = new Location();
+      for (var page in pages) {
+        page.updateLocation(this, locationInfo);
+      }
+    });
+  }
+
+  void doEvent(PageManagerEvent ev) {
+    if (ev == PageManagerEvent.startLoading) {
+      _isLoading = true;
+    } else if (ev == PageManagerEvent.stopLoading) {
+      _isLoading = false;
+    }
+
+    for (var page in pages) {
+      page.updateEvent(this, ev);
+    }
+  }
+
+  bool assignLocation(String url) {
+    if (_isLoading == true) {
+      html.window.location.assign(url);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool assignReplace(String url) {
+    if (_isLoading == true) {
+      html.window.location.replace(url);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Page {
+  bool updateLocation(PageManager manager, Location location) {
+    return true;
+  }
+
+  bool updateEvent(PageManager manager, PageManagerEvent event) {
+    return true;
+  }
+}
