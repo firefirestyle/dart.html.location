5fe266c8c013e5c812c029d45116da0107605b89
kyorohiro <kyorohiro@gmail.com>
2016-10-15 06:09:27 +0900

    ..

--- lib/src/location.dart
+++ lib/src/location.dart
@@ -3,12 +3,20 @@ part of location;
 class Location {
   String _hash = "";
   String _baseAddress = "";
+  String _scheme = "";
+  String _host = "";
+  String _path = "";
+
   Map<String, String> _values = {};
   Map<String, String> _urlValues = {};
   Map<String, String> _hashValues = {};
 
+  String get scheme => _scheme;
   String get hash => _hash;
-  String get baseAddress => _baseAddress;
+  String get host => _host;
+  String get path => _path;
+  String get withSchemeHostPath => _baseAddress;
+  String get baseAddr => "${scheme}://${host}";
   Map<String, String> get values => _values;
   Map<String, String> get urlValues => _urlValues;
   Map<String, String> get hashValues => _hashValues;
@@ -18,6 +26,16 @@ class Location {
     {
       var addr = html.window.location.href.replaceFirst(new RegExp("[#\?].*"), "");
       this._baseAddress = addr.replaceFirst(new RegExp(r"/[^/]*$"), "/");
+    }
+    {
+      this._scheme = this._baseAddress.replaceFirst(new RegExp(r"::.*$"), "");
+    }
+    {
+      var v= this._baseAddress.replaceFirst(new RegExp(r".*::"), "");
+      this._host = v.replaceFirst(new RegExp(r"/.*"), "");
+    }
+    {
+      this._path = this._baseAddress..replaceFirst(new RegExp(r".*/"), "/");
     }
     {
       var prop1 = _hashValues = _prop(html.window.location.hash);
